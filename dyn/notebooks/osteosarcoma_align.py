2cec60e1dce3b30b3d2f5229806cfb58dd9dcfd0
Wanxin Li <w328li@uwaterloo.ca>
2024-05-27 20:31:30 -0700

    added projection_rotation_reparameterization for osteosarcoma dataset

--- dyn/notebooks/osteosarcoma_align.py
+++ dyn/notebooks/osteosarcoma_align.py
@@ -306,6 +306,8 @@ for treatment in TREATMENTS:
         for i, cell in enumerate(cells):
             try:
                 file_path = os.path.join(data_folder, f"{treatment}_{line}_{i}.txt")
+                if os.path.exists(file_path):
+                    continue
                 aligned_cell = align(cell, BASE_CURVE, rescale, rotation, reparameterization, k_sampling_points)
                 np.savetxt(file_path, aligned_cell)
             except:
